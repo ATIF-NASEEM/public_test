3be461040b0eda2e0212bd6b885b0a04448ea178
RMAN <88543131+ATIF-NASEEM@users.noreply.github.com>
2026-02-18 02:51:00 +0500

    Add files via upload

--- main.tf
+++ main.tf
@@ -10,43 +10,43 @@ provider "azurerm" {
 data "azurerm_client_config" "current" {}
 
 # 2. Resource Group
-resource "azurerm_resource_group" "example" {
+resource "azurerm_resource_group" "poc-rg" {
   name     = "kv-vm-test-rg"
   location = "East US"
 }
 
 # 3. Networking
-resource "azurerm_virtual_network" "example" {
+resource "azurerm_virtual_network" "poc-vn" {
   name                = "example-vnet"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.poc-rg.location
+  resource_group_name = azurerm_resource_group.poc-rg.name
 }
 
-resource "azurerm_subnet" "example" {
+resource "azurerm_subnet" "pov-vm-subnet" {
   name                 = "internal"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
+  resource_group_name  = azurerm_resource_group.poc-rg.name
+  virtual_network_name = azurerm_virtual_network.poc-vn.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
 resource "azurerm_network_interface" "example" {
   name                = "example-nic"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.poc-rg.location
+  resource_group_name = azurerm_resource_group.poc-rg.name
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_subnet.example.id
+    subnet_id                     = azurerm_subnet.pov-vm-subnet.id
     private_ip_address_allocation = "Dynamic"
   }
 }
 
 # 4. Key Vault
-resource "azurerm_key_vault" "example" {
+resource "azurerm_key_vault" "poc-kv" {
   name                        = "test-kv-${random_id.id.hex}" # Must be unique
-  location                    = azurerm_resource_group.example.location
-  resource_group_name         = azurerm_resource_group.example.name
+  location                    = azurerm_resource_group.poc-rg.location
+  resource_group_name         = azurerm_resource_group.poc-rg.name
   tenant_id                   = data.azurerm_client_config.current.tenant_id
   sku_name                    = "standard"
 
@@ -61,17 +61,17 @@ resource "azurerm_key_vault" "example" {
 resource "random_id" "id" { byte_length = 4 }
 
 # 5. Create a test secret to retrieve later
-resource "azurerm_key_vault_secret" "example" {
+resource "azurerm_key_vault_secret" "kv-secret" {
   name         = "TestSecret"
   value        = "ConnectivitySuccessful!"
-  key_vault_id = azurerm_key_vault.example.id
+  key_vault_id = azurerm_key_vault.poc-kv.id
 }
 
 # 6. Virtual Machine with Managed Identity
-resource "azurerm_linux_virtual_machine" "example" {
+resource "azurerm_linux_virtual_machine" "pov-mv" {
   name                = "example-vm"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.poc-rg.name
+  location            = azurerm_resource_group.poc-rg.location
   size                = "Standard_B1s"
   admin_username      = "azureuser"
   network_interface_ids = [azurerm_network_interface.example.id]
@@ -98,13 +98,4 @@ resource "azurerm_linux_virtual_machine" "example" {
   identity {
     type = "SystemAssigned"
   }
-}
-
-# 7. Grant the VM Identity access to the Key Vault
-resource "azurerm_key_vault_access_policy" "vm_policy" {
-  key_vault_id = azurerm_key_vault.example.id
-  tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = azurerm_linux_virtual_machine.example.identity[0].principal_id
-
-  secret_permissions = ["Get", "List"]
 }
